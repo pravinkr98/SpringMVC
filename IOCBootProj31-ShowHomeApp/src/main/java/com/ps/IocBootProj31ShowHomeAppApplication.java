5714f30c261237ce528675533256e8907f291fc8
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-05-18 12:16:36 +0530

    Boot-HomePage

---
+++ IOCBootProj31-ShowHomeApp/src/main/java/com/ps/IocBootProj31ShowHomeAppApplication.java
@@ -0,0 +1,13 @@
+package com.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocBootProj31ShowHomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocBootProj31ShowHomeAppApplication.class, args);
+	}
+
+}
---
+++ IOCBootProj31-ShowHomeApp/src/main/java/com/ps/controller/ShowHomeController.java
@@ -0,0 +1,13 @@
+package com.ps.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShowHomeController {
+
+	@RequestMapping("/welcome.htm")
+	public String showHome() {
+		return "home_page";
+	}
+}
